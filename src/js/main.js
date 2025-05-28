ab631d5703545418b2d8776d7abc7e3bb5db712b
camposmm <mcamposdossantos@byupathway.edu>
2025-05-28 15:28:58 -0300

    week02

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
 
-const dataSource = new ProductData("tents");
-const listElement = document.querySelector(".product-list");
-
-const productList = new ProductList("tents", dataSource, listElement);
+const dataSource = new ProductData('tents');
+const listElement = document.querySelector('.product-list'); // Ensure this selector matches your HTML
+const productList = new ProductList('tents', dataSource, listElement);
 productList.init();
