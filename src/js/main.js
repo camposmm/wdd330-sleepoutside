0d756247d0ae14334c66ba33b10d557cf7fa4104
camposmm <mcamposdossantos@byupathway.edu>
2025-05-28 20:49:28 -0300

    new

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ProductList from './ProductList.mjs';
+import ProductData from './ProductData.mjs'; 
+
+const dataSource = new ProductData('tents');
+const listElement = document.querySelector('.product-list');
+const productList = new ProductList('tents', dataSource, listElement);
+productList.init();
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -12,19 +10,6 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
       },
     },
   },
